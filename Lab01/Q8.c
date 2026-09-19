3769ec751fd6cfb38618f1b4dfeb1ed868291334
saltbox-svnit <i26ai052@aid.svnit.ac.in>
2026-09-19 11:45:32 +0530

    Create Q8.c

---
+++ Lab01/Q8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    int t = 31558150;
+    int h, m, s;
+    h = t/3600;
+    m = (t - h*3600)/60;
+    s = t - h*3600 - m*60;
+    printf("time in hrs, min and sec are : %d %d %d", h, m, s);
+}
