2eac40572bf7049b141da65bfc819e01be932a01
saltbox-svnit <i26ai052@aid.svnit.ac.in>
2026-09-19 12:05:13 +0530

    Create Q22.c

---
+++ Lab01/Q22.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+    int n, i, f = 1;
+    printf("Enter a number ");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+        f = f*i;
+    }
+    printf("Factorial of the number is %d", f);
+}
